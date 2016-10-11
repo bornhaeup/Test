2499a6c0b036fa1835025ce7b5dbfc74f691edba
bornhaeup <patrick.air@freenet.de>
2016-10-11 18:24:46 +0200

    test_ant

---
+++ Test_Ant/src/Ant.java
@@ -0,0 +1,12 @@
+
+public class Ant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(3,4));
+	}
+	public static double add (double a, double b){
+		return a+b;
+	}
+
+}
---
+++ Test_Ant/src/Test_Ant.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Ant {
+
+	@Test
+	public void test() {
+		assertEquals(7,Ant.add(4, 3),0);
+	}
+
+}
